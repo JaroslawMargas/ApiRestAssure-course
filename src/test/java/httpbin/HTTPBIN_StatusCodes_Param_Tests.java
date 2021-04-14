a526d0c9807e47cfb67ed600bc12f05132faa768
Jaromarg <jaromarg@gmail.com>
2021-04-14 12:53:55 +0200

    BDD status code parametrized

---
+++ src/test/java/httpbin/HTTPBIN_StatusCodes_Param_Tests.java
@@ -0,0 +1,38 @@
+package httpbin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+
+@RunWith(Parameterized.class)
+public class HTTPBIN_StatusCodes_Param_Tests {
+
+    private final String value;
+    private final String expected;
+
+    public HTTPBIN_StatusCodes_Param_Tests(String value, String expected) {
+        this.value = value;
+        this.expected = expected;
+    }
+
+    @Parameterized.Parameters
+    public static List<Object[]> data() {
+        return Arrays.asList(new Object[][]{{"200", "200"}, {"300", "300"}, {"400", "400"}});
+    }
+
+    @Test
+    public void testSearch() {
+        given().
+                pathParam("codes",value).
+                when().
+                get("https://httpbin.org/status/{codes}").
+        then().assertThat().
+        statusCode(Integer.parseInt(expected));
+    }
+
+}
